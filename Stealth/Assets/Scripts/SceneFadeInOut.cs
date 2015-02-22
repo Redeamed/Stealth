9a957b675bf146b7b34133058463f4caa1a819c0
Redeamed <kdavis929@yahoo.com>
2015-02-22 11:04:12 -0700

    Tutorial 105 -update
    lerp working but deactivating the object to stop rendering doesn't work
    without breaking the lerp interestingly enough.  Leaving the object
    active for now.  Will play around with it later.

--- Stealth/Assets/Scripts/SceneFadeInOut.cs
+++ Stealth/Assets/Scripts/SceneFadeInOut.cs
@@ -5,15 +5,12 @@ public class SceneFadeInOut : MonoBehaviour {
 
 	public float fadeSpeed = 1.5f;
     public CanvasRenderer canvasRenderer;
-    private float alpha = 1;
     private bool sceneStarting = true;
-
+    
     void Awake()
     {
         canvasRenderer = GetComponent<CanvasRenderer>();
-        canvasRenderer.SetAlpha(alpha) ;
     }
-
     void Update()
     {
         if (sceneStarting)
@@ -21,23 +18,23 @@ public class SceneFadeInOut : MonoBehaviour {
             StartScene();
 
         }
+        //else { EndScene(); }
     }
 
     void FadeToClear()
     {
-        canvasRenderer.SetAlpha(Mathf.Lerp(alpha, 0, fadeSpeed * Time.deltaTime));
+        canvasRenderer.SetAlpha(Mathf.Lerp(canvasRenderer.GetAlpha(), 0, fadeSpeed * Time.deltaTime));
     }
     void FadeToBlack()
     {
-        canvasRenderer.SetAlpha(Mathf.Lerp(alpha, 255.0f, fadeSpeed * Time.deltaTime));
+        canvasRenderer.SetAlpha(Mathf.Lerp(canvasRenderer.GetAlpha(), 1, fadeSpeed * Time.deltaTime));
     }
     void StartScene()
     {
         FadeToClear();
-        if(canvasRenderer.GetAlpha() <= 0.25f)
+        if(canvasRenderer.GetAlpha() <= 0.05f)
         {
-            alpha = 0;
-            canvasRenderer.SetAlpha(alpha);
+            canvasRenderer.SetAlpha(0);
             //canvasRenderer.gameObject.SetActive(false);
             sceneStarting = false;
         }
@@ -45,10 +42,11 @@ public class SceneFadeInOut : MonoBehaviour {
 
     public void EndScene()
     {
-        canvasRenderer.gameObject.SetActive(true);
+        //canvasRenderer.gameObject.SetActive(true);
+        
         FadeToBlack();
 
-        if (canvasRenderer.GetAlpha() >= 0.95f)
+        if (canvasRenderer.GetAlpha() >= .95f)
         {
             Application.LoadLevel(1);
         }
