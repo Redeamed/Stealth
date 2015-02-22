25e5e984b3bf1c9c6bf8eb2265c9532810cdb819
Redeamed <kdavis929@yahoo.com>
2015-02-22 00:15:24 -0700

    Tutorial 105
    Unity update changes requires a work around.  in doing so encountered an
    unresolved lerping error when trying to fade the alpha channel on a gui
    panel.  Will fix tomorrow. good night.

---
+++ Stealth/Assets/Scripts/SceneFadeInOut.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneFadeInOut : MonoBehaviour {
+
+	public float fadeSpeed = 1.5f;
+    public CanvasRenderer canvasRenderer;
+    private float alpha = 1;
+    private bool sceneStarting = true;
+
+    void Awake()
+    {
+        canvasRenderer = GetComponent<CanvasRenderer>();
+        canvasRenderer.SetAlpha(alpha) ;
+    }
+
+    void Update()
+    {
+        if (sceneStarting)
+        {
+            StartScene();
+
+        }
+    }
+
+    void FadeToClear()
+    {
+        canvasRenderer.SetAlpha(Mathf.Lerp(alpha, 0, fadeSpeed * Time.deltaTime));
+    }
+    void FadeToBlack()
+    {
+        canvasRenderer.SetAlpha(Mathf.Lerp(alpha, 255.0f, fadeSpeed * Time.deltaTime));
+    }
+    void StartScene()
+    {
+        FadeToClear();
+        if(canvasRenderer.GetAlpha() <= 0.25f)
+        {
+            alpha = 0;
+            canvasRenderer.SetAlpha(alpha);
+            //canvasRenderer.gameObject.SetActive(false);
+            sceneStarting = false;
+        }
+    }
+
+    public void EndScene()
+    {
+        canvasRenderer.gameObject.SetActive(true);
+        FadeToBlack();
+
+        if (canvasRenderer.GetAlpha() >= 0.95f)
+        {
+            Application.LoadLevel(1);
+        }
+    }
+}
